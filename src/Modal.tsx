4d3f52bbb82d6946b9a655f405f921620f8d4645
Guillaume LEDUC <gduc.37@gmail.com>
2024-11-06 21:15:18 +0100

    Refactor Modal component to center content and improve accessibility

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -38,8 +38,8 @@ export const Modal = ({state, children, resetForm, className}: ModalProps) => {
     })
     
     return (
-        <div className={`fixed inset-0 bg-white/50 backdrop-blur-sm z-50 ${isOpen ? "block" : "hidden"}`}>
-            <div className={clsx("absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2", className)} id='modal'>
+        <div className={`fixed inset-0 bg-white/50 backdrop-blur-sm z-50 ${isOpen ? "block" : "hidden"} grid place-content-center place-items-center`}>
+            <div className={clsx("transform -translate-x-1/2 -translate-y-1/2", className)} id='modal'>
                 {children}
             </div>
         </div>
